acccd0dc693898b187c979943e6cfa857416d89d
Sienna Lloyd <sienna@linux.com>
2023-08-15 17:54:26 -0600

    fixing build logic
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- prebuild.sh
+++ prebuild.sh
@@ -5,5 +5,7 @@ namespace retrans;
 
 public static class Global {
     public const string SpeechKey = "${SPEECH_KEY}";
+    public const string AzureClientId = "${AZURE_CLIENT_ID}";
+    public const string AzureClientSecret = "${AZURE_CLIENT_SECRET}";
 }
 EOF
